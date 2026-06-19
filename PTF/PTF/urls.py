aa2314673bbd54fa18fd902862f8e573108e822b
Abu Huraira <113813473+ABUHURAIRA114@users.noreply.github.com>
2026-06-19 17:42:59 +0500

    Ar viewer moved to frontend

--- PTF/PTF/urls.py
+++ PTF/PTF/urls.py
@@ -1,3 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from django.conf import settings
 from django.views.static import serve
 from django.urls import re_path
 
