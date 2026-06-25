edb8c4236d9628fbeca96143e3c66ab892b59063
Abu Huraira <113813473+ABUHURAIRA114@users.noreply.github.com>
2026-06-25 19:54:42 +0500

    forgot save

--- PTF/viewer/admin.py
+++ PTF/viewer/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Scene
+from .models import Response, Scene
 
 admin.site.register(Scene)
-admin.site.register(Review)
+admin.site.register(Response)
